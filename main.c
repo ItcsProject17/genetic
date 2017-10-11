a40f0d1bc4ed1dbfbf0d8027a04b2bd67a7af81b
Jurrian <jurrdb@gmail.com>
2017-10-11 14:49:14 +0200

    Added function that reads the blocks and safeMalloc

--- main.c
+++ main.c
@@ -13,6 +13,34 @@
 #endif
 #include <assert.h>
 
+/* Memory allocating function which indicates it if it fails.*/
+void *safeMalloc(int n) {
+    void *p = malloc(n);
+    if (p == NULL) {
+        printf("Error: malloc(%d) failed. Out of memory?\n", n);
+        exit(EXIT_FAILURE);
+    }
+    return p;
+}
+
+/* Expects an integer n determining the amount of blocks followed by n integers
+ * indicating the sizes of each block. Returns the pointer to an array
+ * containing all the block heights.*/
+int *readBlocks(){
+	int numBlocks;
+	scanf("%d", &numBlocks);
+	int *blocks;
+	blocks = safeMalloc(numBlocks * sizeof(int));
+	
+	for (int i=0; i<numBlocks; i++){
+		scanf("%d", (numBlocks+i));
+	}
+	
+	return blocks;
+}
+
+
 int main(int argc, char *argv[]) {
-	printf("Hello World!");
+	int *blocks;
+	blocks = readBlocks();
 }
