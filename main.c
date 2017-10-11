411f3d49a86211464115439e67492ecf0d7b5c80
Jurrian <jurrdb@gmail.com>
2017-10-11 15:07:27 +0200

    Made sure numBlocs is available in main by passing pointers to readBlocks.

--- main.c
+++ main.c
@@ -23,24 +23,25 @@ void *safeMalloc(int n) {
     return p;
 }
 
-/* Expects an integer n determining the amount of blocks followed by n integers
- * indicating the sizes of each block. Returns the pointer to an array
- * containing all the block heights.*/
-int *readBlocks(){
-	int numBlocks;
-	scanf("%d", &numBlocks);
-	int *blocks;
-	blocks = safeMalloc(numBlocks * sizeof(int));
+/* Expects a pointer to an array and a pointer to an integer. Scans for an 
+ * integer n determining the amount of blocks followed by n integers indicating 
+ * the sizes of each block. Stores the amount of blocks and the sizes in 
+ * the inputted pointer locations.*/
+void readBlocks(int **blocks, int *numBlocks){
+	scanf("%d", numBlocks);
 	
-	for (int i=0; i<numBlocks; i++){
-		scanf("%d", (numBlocks+i));
+	*blocks = safeMalloc(*numBlocks * sizeof(int));
+	
+	for (int i=0; i<*numBlocks; i++){
+		scanf("%d", (*blocks+i));
 	}
 	
-	return blocks;
 }
 
 
 int main(int argc, char *argv[]) {
 	int *blocks;
-	blocks = readBlocks();
+	int numBlocks;
+	readBlocks(&blocks, &numBlocks);
+	printf("numBlocks: %d\nblocks[1-3]: %d %d %d\n", numBlocks, blocks[0], blocks[1], blocks[2]); // debug statement
 }
