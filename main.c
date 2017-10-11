3f6dc12c057eda7569b984d5d4b014ac26573572
Jurrian <jurrdb@gmail.com>
2017-10-11 13:54:37 +0200

    added main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+/* file:	skeleton.c												*/
+/* author:	Jurrian (j.h.d.de.boer@student.rug.nl)					*/
+/* date:	06-09-17												*/
+/* version:	1.0														*/
+/* description:														*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if 1 /*0 for assertions on, 1 for assertions off*/
+	#define NDEBUG
+#endif
+#include <assert.h>
+
+int main(int argc, char *argv[]) {
+	
+}
